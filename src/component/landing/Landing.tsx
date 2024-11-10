3dcea87447ec9d9ed8d5ce0aebe7d47fe8a69b04
ramisa-zahara <ramisazahara@gmail.com>
2024-11-10 13:33:15 +0600

    [ui]:added add task page

--- src/component/landing/Landing.tsx
+++ src/component/landing/Landing.tsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import { Button } from "antd";
+import React from "react";
+import { PlusOutlined } from "@ant-design/icons";
+import { NavLink } from "react-router-dom";
 
-import styles from './Landing.module.scss';
+// import styles from "./Landing.module.scss";
 
-import logo from './landing.svg';
+// import logo from "./landing.svg";
 
 const Landing = () => (
-  <div>
-
-    <header>
+  <div className="bg-white max-h-screen pb-4">
+    {/* <header>
       <img src={logo} className="animate-spin h-10" alt="logo" />
-      <div className="text-red-600">
-        React + TS + Vite + Tailwind
-      </div>
-      <div className={styles.sassExample}>
-        sass is here for styling
-      </div>
+      <div className="text-red-600">React + TS + Vite + Tailwind</div>
+      <div className={styles.sassExample}>sass is here for styling</div>
+    </header> */}
+    <header className="flex justify-between md:px-24 items-center">
+      <span className="text-[#3B3838] font-bold text-2xl">My Tasks</span>
+      <NavLink
+        to="/addtasks"
+        className="text-white bg-[#3B3838] px-4 py-2 rounded-xl flex items-center"
+      >
+        <PlusOutlined />
+        Add Task
+      </NavLink>
     </header>
   </div>
 );
--- src/layout/CustomLayout.tsx
+++ src/layout/CustomLayout.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import Layout from 'antd/lib/layout';
-import { PageHeader } from '@ant-design/pro-layout';
-import { Link, useNavigate } from 'react-router-dom';
+import React from "react";
+import Layout from "antd/lib/layout";
+import { PageHeader } from "@ant-design/pro-layout";
+import { Link, useNavigate } from "react-router-dom";
 
 const { Header, Content, Footer } = Layout;
 
@@ -9,34 +9,33 @@ const CustomHeader = () => {
   const navigate = useNavigate();
   return (
     <PageHeader
-      className="site-page-header h-full"
+      className="site-page-header h-full "
       title={<Link to="/">Taskly</Link>}
       subTitle="Free, Open-Sourced, Offline, Self-Hosted Task Management Solution"
       onBack={() => navigate(-1)}
-      extra={
-        <>
-          <Link to="/page2">Page 2</Link>
-          <Link to="/page3">Page 3</Link>
-        </>
-      }
+      // extra={
+      //   <>
+      //     <Link to="/page2">Page 2</Link>
+      //     <Link to="/page3">Page 3</Link>
+      //   </>
+      // }
     />
   );
 };
 
 const CustomFooter = () => (
-  <div style={{ textAlign: 'center' }}>
+  <div style={{ textAlign: "center" }}>
     <div>
-      { /* @ts-ignore */}
+      {/* @ts-ignore */}
       {__HEAD_COMMIT_HASH__ ? `Trace: ${__HEAD_COMMIT_HASH__}` : ""}
     </div>
-    <div>
-      &copy; {new Date().getFullYear()} - Industrial Training Web
-    </div>
+    <div>Developed by Ramisa Zahara Matin</div>
+    <div>&copy; {new Date().getFullYear()} - Industrial Training Web</div>
   </div>
 );
 
 interface ILayoutProps {
-  children: any
+  children: any;
 }
 
 // eslint-disable-next-line react/prefer-stateless-function
@@ -45,14 +44,12 @@ class CustomLayout extends React.Component<ILayoutProps> {
     const { children } = this.props;
 
     return (
-      <Layout style={{ minHeight: '100vh' }}>
-        <Header style={{ backgroundColor: 'white' }}>
+      <Layout style={{ minHeight: "100vh" }}>
+        <Header style={{ backgroundColor: "white" }}>
           <CustomHeader />
         </Header>
-        <Content>
-          {children}
-        </Content>
-        <Footer style={{ backgroundColor: 'white' }}>
+        <Content>{children}</Content>
+        <Footer style={{ backgroundColor: "white" }}>
           <CustomFooter />
         </Footer>
       </Layout>
