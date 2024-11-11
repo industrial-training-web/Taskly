dc8e6b73497013651ee362f21dbfe741f005700c
ramisa-zahara <ramisazahara@gmail.com>
2024-11-11 15:41:03 +0600

    [ui]: Added tasks list

--- src/component/landing/Landing.tsx
+++ src/component/landing/Landing.tsx
@@ -5,6 +5,7 @@ import { NavLink } from "react-router-dom";
 const onCheck = (e) => {
   console.log(`checked = ${e.target.checked}`);
 };
+
 // import styles from "./Landing.module.scss";
 
 // import logo from "./landing.svg";
@@ -16,7 +17,7 @@ const Landing = () => (
       <div className="text-red-600">React + TS + Vite + Tailwind</div>
       <div className={styles.sassExample}>sass is here for styling</div>
     </header> */}
-    <header className="flex justify-between  items-center pb-16">
+    <header className="flex justify-between  items-center pb-10">
       <span className="text-[#3B3838] font-bold text-2xl">My Tasks</span>
       <NavLink
         to="/addtasks"
@@ -26,6 +27,36 @@ const Landing = () => (
         Add Task
       </NavLink>
     </header>
+    {/* task-card */}
+    <div className="grid gap-5">
+      {/* tasks */}
+      {[1, 2, 3].map((data) => (
+        <div
+          key={data}
+          className="flex justify-between border solid border-[#D9D9D9] rounded-xl bg-[#F5F5F5]"
+        >
+          <div className="flex items-start gap-2 py-7 px-7">
+            <Checkbox onChange={onCheck}></Checkbox>
+            <div className="grid gap-2">
+              <h3 className="font-semibold text-[#3B3838] text-xl">
+                Home Work
+              </h3>
+              <span className="text-[#625E5E] text-[17px] ">
+                Do complete questions 1 and 2
+              </span>
+              <span className="text-[#a9a8a8] font-">Due 15 Feb,2025</span>
+            </div>
+          </div>
+          <div className="py-7 px-7">
+            <EditOutlined
+              style={{ color: "#959090", fontSize: "22px" }}
+              className="px-1"
+            />
+            <DeleteOutlined style={{ color: "#FB7474", fontSize: "22px" }} />
+          </div>
+        </div>
+      ))}
+    </div>
   </div>
 );
 
