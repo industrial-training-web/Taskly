d52f04124419d344bd14faf8f9668cc5d67b3690
maifeeulasad <maifeeulasad@users.noreply.github.com>
2024-11-23 07:34:01 +0000

    deploy: ce75e7a2d5477ad56c0a7062b0cd30ef23d9b1f5

--- assets/Page2.8540c6f0.js
+++
@@ -1,1 +0,0 @@
-import{I as a}from"./index.3e104ae1.js";const o=()=>a("div",{children:"Page 2"});export{o as default};
---
+++ assets/Page2.c789509d.js
@@ -0,0 +1,1 @@
+import{I as a}from"./index.8129968d.js";const o=()=>a("div",{children:"Page 2"});export{o as default};
