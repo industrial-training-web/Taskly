2b907bd5e32d861fd357294fafae9c85808bc6ee
ramisa-zahara <ramisa-zahara@users.noreply.github.com>
2024-12-03 17:27:40 +0000

    deploy: e09fc35565215fb07312a8c654dba9f1c86ffd50

--- assets/Page2.c789509d.js
+++
@@ -1,1 +0,0 @@
-import{I as a}from"./index.8129968d.js";const o=()=>a("div",{children:"Page 2"});export{o as default};
---
+++ assets/Page2.fc1f9889.js
@@ -0,0 +1,1 @@
+import{I as a}from"./index.d05b6907.js";const o=()=>a("div",{children:"Page 2"});export{o as default};
